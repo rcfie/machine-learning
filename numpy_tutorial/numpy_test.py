2261be72065c87cadfe7edd4183e71150d568d5c
Nathan Perkins <nperkins487@gmail.com>
2016-07-04 21:58:49 -0700

    pandas tutorial

--- numpy_tutorial/numpy_test.py
+++ numpy_tutorial/numpy_test.py
@@ -15,10 +15,10 @@ lists in Python, except that every thing inside an array must be of the
 same type, like int or float.
 '''
 # Change False to True to see Numpy arrays in action
-if True
+if False:
     array = np.array([1, 4, 5, 8], float)
     print array
-    print
+    print ""
     array = np.array([[1, 2, 3], [4, 5, 6]], float)  # a 2D arrayMatrix
     print array
 
@@ -27,50 +27,50 @@ You can index, slice, and manipulate a Numpy array much like you would with a
 a Python list.
 '''
 # Change False to True to see array indexing and slicing in action
-if False
+if False:
     array = np.array([1, 4, 5, 8], float)
     print array
-    print
+    print ""
     print array[1]
-    print
+    print ""
     print array[2]
-    print
+    print ""
     array[1] = 5.0
     print array[1]
 
 # Change False to True to see Matrix indexing and slicing in action
-if False
+if False:
     two_D_array = np.array([[1, 2, 3], [4, 5, 6]], float)
     print two_D_array
-    print
+    print ""
     print two_D_array[1][1]
-    print
-    print two_D_array[1, ]
-    print
-    print two_D_array[, 2]
+    print ""
+    print two_D_array[1, :]
+    print ""
+    print two_D_array[:, 2]
 
 '''
 Here are some arithmetic operations that you can do with Numpy arrays
 '''
 # Change False to True to see Array arithmetics in action
-if False
+if False:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([5, 2, 6], float)
     print array_1 + array_2
-    print
+    print ""
     print array_1 - array_2
-    print
-    print array_1  array_2
+    print ""
+    print array_1 * array_2
 
 # Change False to True to see Matrix arithmetics in action
-if False
+if False:
     array_1 = np.array([[1, 2], [3, 4]], float)
     array_2 = np.array([[5, 6], [7, 8]], float)
     print array_1 + array_2
-    print
+    print ""
     print array_1 - array_2
-    print
-    print array_1  array_2
+    print ""
+    print array_1 * array_2
 
 '''
 In addition to the standard arthimetic operations, Numpy also has a range of
@@ -79,10 +79,10 @@ mean and dot product.
 
 Both of these functions will be useful in later programming quizzes.
 '''
-if False
+if True:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([[6], [7], [8]], float)
     print np.mean(array_1)
     print np.mean(array_2)
-    print
+    print ""
     print np.dot(array_1, array_2)
